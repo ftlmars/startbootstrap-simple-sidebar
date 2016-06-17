49bc48eb69136561b1a7d141a7850af140940b0f
ftlmars <ftlmars@users.noreply.github.com>
2016-06-17 11:19:29 +0300

    Create SidebarAsset.php

---
+++ yii2/web/SidebarAsset.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ftlmars\sidebar\yii2\web;
+
+use yii\web\AssetBundle;
+
+class SidebarAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/ftlmars/startbootstrap-simple-sidebar';
+
+    public $css = [
+        'css/simple-sidebar.css',
+    ];
+
+}
