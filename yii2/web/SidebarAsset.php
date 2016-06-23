27577631cb484b26ac7c7daae14aaf13b26fd3b0
Maxym Semenykhin <semenihin.maksim@gmail.com>
2016-06-23 09:35:40 +0300

    toggle icon added | missing .css fix

--- yii2/web/SidebarAsset.php
+++ yii2/web/SidebarAsset.php
@@ -10,7 +10,7 @@ class SidebarAsset extends AssetBundle
 
     public $css = [
         'css/simple-sidebar.css',
-        'css/simple-sidebar-overlay',
+        'css/simple-sidebar-overlay.css',
     ];
 
 }
